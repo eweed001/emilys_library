9803fa9bba8d6136f89f38bcb1cdff099b08337c
Emily Weed <emw232@cornell.edu>
2021-06-30 16:56:36 -0400

    added more pages and starting working on sessions

--- emilyslibrary/catalog/views.py
+++ emilyslibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre, Review
+from django.views import generic
 
 
 def index(request):
@@ -14,11 +15,33 @@ def index(request):
 
     num_authors = Author.objects.count()
 
+    # number of visits to this view, as counted in the session variable
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_avail': num_instances_avail,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
